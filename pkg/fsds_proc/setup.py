75167202a3ecd592239c3ea175bfa21492825fdd
glitt13 <guy.litt@noaa.gov>
2024-07-11 14:03:53 -0600

    feat: move standardized metrics categories file into fsds_proc package, add hydrotools metrics to standardized metrics categories

--- pkg/fsds_proc/setup.py
+++ pkg/fsds_proc/setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
+# Instructions on package data files: https://sinoroc.gitlab.io/kb/python/package_data.html
 # Instructions for build & install in terminal:
-# > cd /path/to/proc_fsds/
+# > cd /path/to/fsds_proc/
 # > python setup.py sdist bdist_wheel
 # > pip install -e ./
 setup(
+    include_package_data = True,
+    #package_data = {'fsds_proc' : ['data/*.yaml']},
     name="fsds_proc",
-    version="0.1.2",
+    version="0.1.3",
     author="Guy Litt",
     author_email="guy.litt@noaa.gov",
     description="A simple package for processing data in the formulation selection decision support tool",

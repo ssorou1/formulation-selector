e674e541f83f846ae7ad44ec642fc8d6f840ab91
bolotinl <bolotinljb@gmail.com>
2024-07-18 11:16:03 -0700

    Follow Google R Style guide, add comparison of two methods

--- scripts/developing/Assign_ComIDs.R
+++ scripts/developing/Assign_ComIDs.R
@@ -3,27 +3,23 @@
 ## Code author: L. Bolotin
 #===============================================================================
 
-## Call packages
-# x <- c("sf", "rgdal", "raster", "tidyverse", "nhdplusTools", "beepr", "dataRetrieval")
-# lapply(x, require, character.only = TRUE)
-# rm(x)
-library(tidyverse)
-library(nhdplusTools)
-library(dataRetrieval)
-library(beepr)
-library(sf)
+# Call packages
+x <- c("sf", "tidyverse", "nhdplusTools", "beepr", "dataRetrieval")
+lapply(x, require, character.only = TRUE)
+rm(x)
 
 ## Set working directory to a file with the CAMELS IDs
 setwd('/Users/laurenbolotin/Lauren/FSDS/results/')
 
+#--------------------------------------------
+## Bring in site data
+#--------------------------------------------
 dat <- read.table('camels_hydro.txt', sep = ";", header = TRUE)
 
-#--------------------------------------------
-## Bring in location data
-#--------------------------------------------
-sites <- unique(zeroPad(dat$gauge_id, 8))
-
 ## Format
+sites <- unique(zeroPad(dat$gauge_id, 8))
+rm(dat)
+
 sites <- paste0("USGS-", sites)
 
 ## Prepare a data frame where we will put the ComID's
@@ -34,38 +30,44 @@ sites <- levels(sites) |>
 sites$COMID <- "" # leave blank for now, this is what we are going to populate
 colnames(sites)[1] <- "GAGE_ID"
 
-## It seems (after trial) that using a USGS ID to identify a COMID is preferable to using coordinates, so do that whenever possible
-## As opposed to using the lat/long for the Rio Grande sites, use the USGS ID's for the nearest streamgauges
-# Re-name the sites to these gauges and then change the names back after assigning the ComID
+## It seems (after trial) that using a USGS ID to identify a COMID is preferable 
+## to using coordinates, so do that whenever possible
 
 #--------------------------------------------
 ## Assign NHD ComID using USGS SiteID
 #--------------------------------------------
 
 ## Create function
-findCOMID_USGS_ID <- function(x){ # x = USGS SiteID
-  nldi_nwis <- list(featureSource = "nwissite", featureID = x)
-  (sites$COMID[which(sites$GAGE_ID == x )] <<- discover_nhdplus_id(nldi_feature = nldi_nwis))
+findComidUsgsID <- function(x){ # x = USGS SiteID
+  tryCatch(nldi_nwis <- list(featureSource = "nwissite", featureID = x), error = function(e) NULL)
+  tryCatch(sites$COMID[which(sites$GAGE_ID == x )] <<- discover_nhdplus_id(nldi_feature = nldi_nwis), error = function(e) NULL)
 }
-lapply(sites$GAGE_ID, findCOMID_USGS_ID)
+lapply(sites$GAGE_ID, findComidUsgsID)
 beep() # Notifies us when the function above is done running with a sound
-
+usgs_comids <- sites
 
 #--------------------------------------------
 ## Assign NHD ComID using lat/long
 #--------------------------------------------
-siteNumbers <- substr(sites$GAGE_ID, 6, 14)
-site_metadata <- readNWISsite(siteNumbers) |> select(c(site_no, dec_lat_va, dec_long_va))
+site_numbers <- substr(sites$GAGE_ID, 6, 14)
+site_metadata <- readNWISsite(site_numbers) |> select(c(site_no, dec_lat_va, dec_long_va))
 
 ## Create function
-findCOMID_USGS_coords <- function(x){ # x = USGS GAGE_ID
+findComidCoords <- function(x){ # x = USGS GAGE_ID
   tryCatch(point <- st_sfc(st_point(c((site_metadata$dec_long_va[which(site_metadata$site_no == substr(x, 6, 14))]), (site_metadata$dec_lat_va[which(site_metadata$site_no == substr(x, 6, 14))]))), crs = 4269), error = function(e) NULL)
   tryCatch(sites$COMID[which(sites$GAGE_ID == x)] <<- discover_nhdplus_id(point), error = function(e) NULL)
 }
-lapply(sites$GAGE_ID, findCOMID_USGS_coords)
+lapply(sites$GAGE_ID, findComidCoords)
 beep()
+coords_comids <- sites
+rm(sites, site_numbers, site_metadata)
 
 #----------------------------------------------
 ## Compare ComID's assigned by the two methods
 #----------------------------------------------
+comp_comids <- merge(coords_comids, usgs_comids, by = 'GAGE_ID')
+n_matches = sum(comp_comids$COMID.x == comp_comids$COMID.y)
+n_mismatches <-  nrow(comp_comids) - n_matches
 
+# TODO: Add some sort of functionality that checks ComIDs assigned by lat/long
+# to make sure that the ComID it chooses is actually in the same drainage area

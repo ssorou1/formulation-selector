11960eadb4c8501c956a06b33e36f6f5501d0348
bolotinl <bolotinljb@gmail.com>
2024-08-09 12:49:41 -0700

    Functional attribute checker; ongoing improvements

--- pkg/proc.attr.hydfab/flow/fsds_attrs_select.R
+++ pkg/proc.attr.hydfab/flow/fsds_attrs_select.R
@@ -15,46 +15,51 @@
 library(yaml)
 
 # Read in the menu of attributes available through FSDS
-attr_menu <- yaml::read_yaml('./fsds_attr_menu_reformat.yaml')
 # attr_menu <- yaml::read_yaml('./fsds_attr_menu.yaml')
-# attr_menu <- yaml::read_yaml('./fsds_attr_menu_nocat.yaml')
+attr_menu <- yaml::read_yaml('./fsds_attr_menu_nocat.yaml')
 
-# 
-# idxs_vars <- base::lapply(attr_menu$hydroatlas_attributes, function(x) names(x)) %>%
-#   base::unlist()
-# 
-# ls_vars <- base::sapply(idxs_vars, function(x) raw_config$attr_select[[x]])
 
 # test <- unlist(attr_menu$hydroatlas_attributes$hydrology) # pulls pair of var name and description
-test <- names(attr_menu$hydroatlas_attributes$hydrology) # pulls var name only
+# test <- names(attr_menu$hydroatlas_attributes$hydrology) # pulls var name only
 
 # Read in the user defined config of attributes of interest
 # attr_cfg <- yaml::read_yaml('../../scripts/eval_ingest/xssa/xssa_attr_config_reformat2.yaml')
 attr_cfg <- yaml::read_yaml('../../scripts/eval_ingest/xssa/xssa_attr_config.yaml')
 
-# TODO: I REALLY suggest reformatting the YAML files so no indexing by numbers is necessary. 
-# This can be done by removing the bullet points " - " in each line and relying only on indentation.
-# That way this workflow will not rely on things being in a particular order
 # Determine which data sets the user specifies attributes from
-ha <- attr_cfg[["attr_select"]][[1]][["ha_vars"]]
-ha_sel <- base::ifelse((base::length(ha) > 0) & !base::is.null(ha[1][[1]]), 
-                 TRUE, FALSE)
-usgs <- attr_cfg[["attr_select"]][[2]][["usgs_vars"]]
-usgs <- base::ifelse((base::length(usgs) > 0) & !base::is.null(usgs[1][[1]]), 
-               TRUE, FALSE)
-sc <- attr_cfg[["attr_select"]][[3]][["sc_vars"]]
-sc <- base::ifelse((base::length(sc) > 0) & !base::is.null(sc[1][[1]]),
-             TRUE, FALSE)
+ha_vars_sel <- base::lapply(attr_cfg$attr_select, function(x) names(x)) %>%
+  base::unlist() %>% base::grep(pattern = "ha_vars")
+ha_vars_sel <- base::sapply(ha_vars_sel, function(x) attr_cfg$attr_select[[x]]) |> 
+  unlist() |> 
+  unname() |>
+  tolower()
+
+usgs_vars_sel <- base::lapply(attr_cfg$attr_select, function(x) names(x)) %>%
+  base::unlist() %>% base::grep(pattern = "usgs_vars")
+usgs_vars_sel <- base::sapply(usgs_vars_sel, function(x) attr_cfg$attr_select[[x]]) |> 
+  unlist() |> 
+  unname() |>
+  tolower()
+
+sc_vars_sel <- base::lapply(attr_cfg$attr_select, function(x) names(x)) %>%
+  base::unlist() %>% base::grep(pattern = "sc_vars")
+sc_vars_sel <- base::sapply(sc_vars_sel, function(x) attr_cfg$attr_select[[x]]) |> 
+  unlist() |> 
+  unname() |>
+  tolower()
+
+vars_sel <- c(ha_vars_sel, usgs_vars_sel, sc_vars_sel) # camels_vars_sel
+rm(ha_vars_sel, usgs_vars_sel, sc_vars_sel, camels_vars_sel)
 
 # Check if the entered variables exist in the attribute menu
-ha_menu <- base::tolower(attr_menu$hydroatlas_attributes)
+ha_menu <- unlist(attr_menu$hydroatlas_attributes) |> names() |> tolower()
+# usgs_menu <- unlist(attr_menu$usgs_attributes) |> names() |> tolower()
+# sc_menu <- unlist(attr_menu$sc_attributes) |> names() |> tolower()
+camels_menu <- unlist(attr_menu$camels_attributes) |> names() |> tolower()
+vars_menu <- c(ha_menu, camels_menu) # sc_menu, usgs_menu
+rm(ha_menu, usgs_menu, sc_menu, camels_menu)
 
-attr_select <- attr_cfg$attr_select
-
-ha_sel <- base::tolower(attr_select[[1]]$ha_vars)
-usgs_sel <- base::tolower(attr_select[[2]]$usgs_vars)
-sc_sel <- base::tolower(attr_select[[3]]$sc_vars)
-
-ha_var_menu <- base::names(base::unlist(attr_menu$hydroatlas_attributes))
-camels_var_menu <- base::names(base::unlist(attr_menu$camels_attributes))
-
+vars_sel %in% vars_menu
+missing_vars <- which(!vars_sel %in% vars_menu)
+print('WARNING: the following attributes, as specified, were not found in the attribute menu:')
+print(vars_sel[missing_vars])

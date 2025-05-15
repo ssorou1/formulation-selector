504854ee70035a5f3a4a90eca693a48c48ca4c14
Soroush Sorourian <ssorourian@lynker.com>
2025-05-15 11:17:58 -0600

    create a py file for all the schemas

---
+++ scripts/eval_ingest/xssa/schemas.py
@@ -0,0 +1,53 @@
+import pandera as pa
+from pandera import Column, DataFrameSchema, Index, Check
+
+    # %% Introducing DataFrameSchema for dataframe objects
+schema_df_attr = DataFrameSchema({
+        "featureID": Column(pa.Object, nullable=False),  # accepts int or str
+        "featureSource": Column(str,checks=pa.Check.isin(["COMID", "custom_hfuid"]),nullable=False),
+        "data_source": Column(str,checks=pa.Check.isin(["hydroatlas__v1", "usgs_nhdplus__v2"]),nullable=False),
+        "dl_timestamp": Column(pa.DateTime,nullable=False),
+        "attribute": Column(str,checks=pa.Check.str_length(1, 30),nullable=False),
+        "value": Column(float,nullable=False),
+    },
+    index=Index(int, name=None),coerce=True,strict=True,name="DFAttr"
+)
+
+
+wkt_point_pattern = r"^POINT\s*\(\-?\d+(\.\d+)?\s+\-?\d+(\.\d+)?\)$"    
+schema_gdf_comid = DataFrameSchema({
+        "comid": Column(int, nullable=False),
+        "gage_id": Column(int, nullable=False),
+        "geometry": Column(str,checks=pa.Check.str_matches(wkt_point_pattern),nullable=False)
+    },
+    index=Index(int),coerce=True,strict=True,name="GDFComid"
+)
+
+
+schema_rslt_eval_df = pa.DataFrameSchema({
+        "algorithm": Column(pa.String,checks=Check.isin(["rf", "mlp"]),nullable=False),
+        "type": Column(pa.String,checks=Check.isin(["random forest regressor", "multi-layer perceptron regressor"]),nullable=False),
+        "metric": Column(pa.String,checks=Check.isin(["NSE", "RMSE", "KGE"]),nullable=False),
+        "mse": Column(pa.Float,nullable=False),
+        "r2": Column(pa.Float,nullable=False),
+        "dataset": Column(pa.String,nullable=False),
+        "file_pipe": Column(pa.String,checks=Check.str_matches(r".+\.joblib$"),nullable=False),
+        "algo": Column(pa.String,checks=Check.isin(["rf", "mlp"]),nullable=False),
+    },
+    index=pa.Index(pa.Int),coerce=True,strict=True,name="RsltEvalDF"
+)
+
+schema_attrs_sel = DataFrameSchema({
+        0: Column(pa.String,nullable=False),
+        },
+    index=pa.Index(pa.Int),coerce=True,strict=True,name="AttrsSelDF"
+)
+
+schema_dat_resp = pa.DataFrameSchema({
+    "basin_name": Column(str, nullable=False),
+    "RMSE": Column(float, nullable=False),
+    "NSE": Column(float, nullable=False),
+    "KGE": Column(float, nullable=False),
+    "gage_id": Column(int, nullable=False),
+    "comid": Column(int, nullable=False),
+})
